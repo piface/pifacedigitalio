c61c3e8221d38ebb25da0f81c55acf4fbef3500a
Thomas Preston <thomas.mark.preston@gmail.com>
2013-02-15 11:18:43 +0000

    Using GPLv2 now

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='pifacedigitalio',
 	description='The PiFace Digital Input/Output module.',
 	author='Thomas Preston',
 	author_email='thomasmarkpreston@gmail.com',
-	license='GPLv3',
+	license='GPLv2',
 	url='http://pi.cs.man.ac.uk/interface.htm',
 	py_modules=['pifacedigitalio'],
 )
