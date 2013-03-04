8129d64ac15d971124ad21b68d40ae72aeb5c858
Thomas Preston <thomasmarkpreston@gmail.com>
2013-03-04 14:29:36 +0000

    updated to gplv3

--- pifacedigitalio.py
+++ pifacedigitalio.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-"""
-pifacedigitalio.py
-Provides I/O methods for interfacing with PiFace Digital (on the Raspberry Pi)
+"""Provides I/O methods for interfacing with PiFace Digital (for Raspberry Pi)
 Copyright (C) 2013 Thomas Preston <thomasmarkpreston@gmail.com>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,8 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
 import ctypes
--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
-
 from distutils.core import setup, Extension
 
 DISTUTILS_DEBUG=True
 
 setup(name='pifacedigitalio',
-	version='1.0',
+	version='1.1',
 	description='The PiFace Digital Input/Output module.',
 	author='Thomas Preston',
 	author_email='thomasmarkpreston@gmail.com',
-	license='GPLv2+',
+	license='GPLv3+',
 	url='http://pi.cs.man.ac.uk/interface.htm',
 	py_modules=['pifacedigitalio'],
 )
