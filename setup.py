76c8a2eeb52bbd224fe5aaeb580012a6bf9a0773
Thomas Preston <thomasmarkpreston@gmail.com>
2013-07-15 16:05:47 +0100

    Inheritance, not polymorphism

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     py_modules=['pifacedigitalio'],
     long_description=open('README.md').read() + open('CHANGELOG').read(),
     classifiers=[
-        "License :: OSI Approved :: GNU Affero General Public License v3 or "
+        "License :: OSI Approved :: GNU Affero General Public License v3 or ",
         "later (AGPLv3+)",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 2",
