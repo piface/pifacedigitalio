b20f309bc5f4d1cd11a0a3ce4a4ec383124ad8be
Thomas Preston <thomasmarkpreston@gmail.com>
2013-09-26 16:40:42 +0100

    added dpkg files

---
+++ makedebpkg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd docs/
+make html
+cd -
+
+python setup.py --command-packages=stdeb.command sdist_dsc
+
+version=$(cat pifacedigitalio/version.py | sed 's/.*\([0-9]\.[0-9]\.[0-9]\).*/\1/')
+cd deb_dist/pifacedigitalio-$version/
+
+cp {../../dpkg-files,debian}/control
+cp {../../dpkg-files,debian}/copyright
+cp {../../dpkg-files,debian}/rules
+cp {../../dpkg-files,debian}/python-pifacedigitalio.install
+cp {../../dpkg-files,debian}/python3-pifacedigitalio.install
+
+ls ../../examples/ | while read example
+do
+    echo ../../examples/$example >> debian/python-pifacedigitalio.examples
+    echo ../../examples/$example >> debian/python3-pifacedigitalio.examples
+done
+
+dpkg-buildpackage #-us -uc
